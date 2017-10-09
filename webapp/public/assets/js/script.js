94ac88bb3fb244d526bda26e43ba2310f3c6c1c0
Endri Kastrati <endriau@gmail.com>
2017-10-09 18:24:56 +1100

    completed a few changes to the user-interface

--- webapp/public/assets/js/script.js
+++ webapp/public/assets/js/script.js
@@ -16,10 +16,16 @@ $(document).ready(function()
     $("#diagnose").on("click",function()
     {
         $("#result").html(""); // clean up any previously displayed results.
-        $("#loader").show(); var list=[]; // start the gif loader.
+        var list=[]; // start the gif loader.
         // read the user provided values from the input attributes.
         $("input[type='text']").each(function() { list.push(this.value); });
         // format the read values as json.
+
+        for (var i=0;i<list.length;i++)
+        {
+            if (!$.isNumeric(list[i])) { alert("Data must be numerical"); return; }
+        }
+
         var data={ c0: list[0], c1: list[1], c2: list[2], c3:list[3], c4: list[4] };
         socket.emit("diagnosis",data);  // Send the input values to the server via sockets.
     });
@@ -28,19 +34,39 @@ $(document).ready(function()
     // output values from the server.
     socket.on("results",function(data)
     {
-        // If a package is received from the sender we will wait a bit
-        // so that the gif is properly displayed (mostly for aesthetic values).
-        setTimeout(function()
+        // Format the received output result and based on it's value
+        // write the corresponding classifcation into the label that
+        // has the id 'result'.
+        var class1="euthyroidism"; var class2="hyperthyroidism";
+        var class3="hypothyroidism"; data.prediction=$.trim(data.prediction);
+
+        if (data.prediction.localeCompare("1 0 0")==0)
         {
-            // Format the received output result and based on it's value
-            // write the corresponding classifcation into the label that
-            // has the id 'result'.
-            var class1="euthyroidism"; var class2="hyperthyroidism";
-            var class3="hypothyroidism"; data.prediction=$.trim(data.prediction);
-            if (data.prediction.localeCompare("1 0 0")==0) { $("#result").html(class1); }
-            if (data.prediction.localeCompare("0 1 0")==0) { $("#result").html(class2); }
-            if (data.prediction.localeCompare("0 0 1")==0) { $("#result").html(class3); }
-            $("#loader").hide(); // Once the result has been written,hide the loader.
-        },1000);
+            $("#result").html(class1);
+            $("#result").removeClass('text-success');
+            $("#result").removeClass('text-danger');
+            $("#result").addClass('text-success');
+            $("#result").attr('href', 'https://en.wikipedia.org/wiki/Euthyroid');
+        }
+
+        if (data.prediction.localeCompare("0 1 0")==0)
+        {
+            $("#result").html(class2);
+            $("#result").removeClass('text-success');
+            $("#result").removeClass('text-danger');
+            $("#result").addClass('text-danger');
+            $("#result").attr('href', 'https://en.wikipedia.org/wiki/Hyperthyroidism');
+        }
+
+        if (data.prediction.localeCompare("0 0 1")==0)
+        {
+            $("#result").html(class3);
+            $("#result").removeClass('text-success');
+            $("#result").removeClass('text-danger');
+            $("#result").addClass('text-danger');
+            $("#result").attr('href', 'https://en.wikipedia.org/wiki/Hypothyroidism');
+        }
+
+        $("#result").attr("target","_blank");
     });
 });
